2c32995ea953e809f2dadfacc356bc7e7c92c00d
Suraj-Nair77 <surnair@deloitte.com>
2026-04-06 14:52:43 +0530

    Delete src directory

--- src/app/api/history/calories/route.ts
+++
@@ -1,8 +0,0 @@
-import{getCalorieHistory} from '@/service/calories'
-export async function GET(){
-    const result = await getCalorieHistory()
-    return Response.json(result)
-}
-
-
-
--- src/app/api/history/workouts/route.ts
+++
@@ -1,5 +0,0 @@
-import {getWorkoutHistory} from '@/service/workouts'
-export async function GET(){
-const result = await getWorkoutHistory()
-return Response.json(result)
-}
